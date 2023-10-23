9f7a7121948099a03d42e6e8992f2341291f8133
mmulyana <mulyan.t20@gmail.com>
2023-10-24 00:21:47 +0700

    style: register

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -7,6 +7,7 @@ import { z } from 'zod'
 
 import { handleRegister } from '@/service/auth'
 import { createUser } from '@/service/user'
+import Link from 'next/link'
 
 type formType = {
   username: string
@@ -56,33 +57,81 @@ export default function Page() {
   }
 
   return (
-    <div>
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label>username</label>
-          <input {...register('username')} />
-          {errors.username && <p>{errors.username.message}</p>}
+    <>
+      <h1 className='text-lg text-center text-white'>Register</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='mt-4 flex flex-col gap-4'
+      >
+        <div className='relative h-fit'>
+          <label className='block text-sm text-white/80 capitalize'>
+            username
+          </label>
+          <input
+            className='w-full mt-0.5 rounded py-2 px-2.5 bg-[#33333e] outline-none text-white'
+            {...register('username')}
+          />
+          {errors.username && (
+            <p className='absolute text-xs text-red-500 -bottom-5'>
+              {errors.username.message}
+            </p>
+          )}
         </div>
-        <div>
-          <label>email</label>
-          <input {...register('email')} />
-          {errors.email && <p>{errors.email.message}</p>}
+        <div className='relative h-fit mt-2.5'>
+          <label className='block text-sm text-white/80 capitalize'>
+            email
+          </label>
+          <input
+            className='w-full mt-0.5 rounded py-2 px-2.5 bg-[#33333e] outline-none text-white'
+            {...register('email')}
+          />
+          {errors.email && (
+            <p className='absolute text-xs text-red-500 -bottom-5'>
+              {errors.email.message}
+            </p>
+          )}
         </div>
-        <div>
-          <label>password</label>
-          <input {...register('password')} />
-          {errors.password && <p>{errors.password.message}</p>}
+        <div className='relative h-fit mt-2.5'>
+          <label className='block text-sm text-white/80 capitalize'>
+            password
+          </label>
+          <input
+            className='w-full mt-0.5 rounded py-2 px-2.5 bg-[#33333e] outline-none text-white'
+            {...register('password')}
+          />
+          {errors.password && (
+            <p className='absolute text-xs text-red-500 -bottom-5'>
+              {errors.password.message}
+            </p>
+          )}
         </div>
-        <div>
-          <label>confirm password</label>
-          <input {...register('confirm')} />
+        <div className='relative h-fit mt-2.5'>
+          <label className='block text-sm text-white/80 capitalize'>
+            confirm password
+          </label>
+          <input
+            className='w-full mt-0.5 rounded py-2 px-2.5 bg-[#33333e] outline-none text-white'
+            {...register('confirm')}
+          />
+          {errors.confirm && (
+            <p className='absolute text-xs text-red-500 -bottom-5'>
+              {errors.confirm.message}
+            </p>
+          )}
         </div>
-        <button className='mt-2' type='submit'>
-          {errors.confirm && <p>{errors.confirm.message}</p>}
+        <button
+          type='submit'
+          className='mt-8 w-full py-2.5 rounded bg-red-600 text-white'
+        >
           Register
         </button>
       </form>
-    </div>
+      <p className='mt-8 text-center text-white text-sm'>
+        Already have an account?{' '}
+        <Link href='/login' className='text-red-500'>
+          Login
+        </Link>
+      </p>
+    </>
   )
 }
